4e464e05b4bdff310512b81897ab0a1c164c0fc5
Vlad <vlad.anitas@appliedtechnology.se>
2023-03-27 09:35:15 +0200

    pre-push

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -8,6 +8,8 @@ public class Restaurant
     [Required] public string Name { get; set; }
     [Required] public string Description { get; set; }
     [Required] public string Location { get; set; }
+    [Required] public string PhoneNumber { get; set; }
+    [Required] public string Email { get; set; }
     [Required] public double DeliveryCost { get; set; }
     [Required] public string MainPictureUrl { get; set; }
     public List<Dish> Dishes { get; set; }
